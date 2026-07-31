7f3e518461bfb2ffd8f127966becbb68c0b32468
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-07-31 16:25:32 -0400

    add User serializer (used in UserResponse)

--- django/conduit/serializers.py
+++ django/conduit/serializers.py
@@ -10,3 +10,15 @@ class NewUser(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ["username", "email", "password"]
+
+# used in UserResponse
+class User(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = ["email", "username", "bio", "image"]
+
+    token = serializers.SerializerMethodField
+
+    def generate_token(self, obj):
+        # TODO
+        return
