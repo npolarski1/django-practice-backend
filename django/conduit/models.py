d470a003977c20faa19248def06235882881e227
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-07-30 21:05:23 -0400

    create article model

--- django/conduit/models.py
+++ django/conduit/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=150, blank=True)
     image = models.CharField(max_length=150, blank=True)
     following = models.ManyToManyField("self", symmetrical=False)
+
+class Article(models.Model):
+    slug = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    body = models.CharField(max_length=100000)
+    tagList = ArrayField(models.CharField(max_length=25), size=4)
+    createdAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
+    favorites_count = models.IntegerField(default=0)
+    author = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
