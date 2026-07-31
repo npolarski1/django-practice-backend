2755e7821d483e85222378dc17b643ceb6e78514
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-07-30 20:32:10 -0400

    create user model

--- django/conduit/models.py
+++ django/conduit/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SiteUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=150, blank=True)
+    image = models.CharField(max_length=150, blank=True)
+    following = models.ManyToManyField("self", symmetrical=False)
