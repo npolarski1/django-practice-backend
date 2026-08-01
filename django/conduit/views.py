4c5cbaffb0fb3889f3b73065910ddd799e6c7bfd
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-01 01:08:04 -0400

    add create_user view

--- django/conduit/views.py
+++ django/conduit/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from psycopg import IntegrityError
 
-# Create your views here.
+import serializers
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(["POST"])
+def create_user(request):
+    serializer = serializers.NewUser(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        # TODO implement error handling (409/422 responses)
+
+        return Response(serializers.User(serializer.data), status=status.HTTP_201_CREATED)
