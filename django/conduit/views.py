3a533cb154e7fac17ab82b4ed6d1453e478e8c0e
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-11 16:12:57 -0400

    add /users/login view

--- django/conduit/views.py
+++ django/conduit/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from models import User
+from django.db.models import Model
 
 @api_view(["POST"])
 def create_user(request):
@@ -17,3 +19,32 @@ def create_user(request):
         # TODO implement error handling (409/422 responses)
 
         return Response(serializers.User(serializer.data), status=status.HTTP_201_CREATED)
+
+# /users/login
+# Login for existing user
+@api_view(["POST"])
+def login(request):
+    # convert json request data to serializer
+    serializer = serializers.LoginUser(data=request.data)
+
+    # validate data matches expected schema
+    if serializer.is_valid():
+        # Pylance doesn't know is_valid() populates validated_data so assert to silence error
+        valid_data = serializer.validated_data
+        assert isinstance(valid_data, dict)
+
+        # check that user with inputted email exists
+        try:
+            user = User.objects.get(email=valid_data.get("email"))
+        # if user doesn't exist, return 401 error code
+        except Model.DoesNotExist:
+            # TODO
+            return Response()
+
+        # check password matches
+        if user.password == valid_data.get("password"):
+            # TODO return with JWT
+            return Response()
+        else:
+            # TODO return 408 error code
+            return Response()
