ecbdba41d818eac6bacd7d63f4f3c3f1b11d1215
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-20 17:05:08 -0400

    fix imports for conduit.serializers and conduit.models.User

--- django/conduit/views.py
+++ django/conduit/views.py
@@ -1,12 +1,12 @@
 from psycopg import IntegrityError
 
-import serializers
+import conduit.serializers as serializers
 
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from models import User
+from conduit.models import User
 from django.db.models import Model
 
 # /users
