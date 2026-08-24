a71a12480472a0a2ca59382affd96e9366098f42
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-24 15:09:43 -0400

    remove trailing slashes from url routes

--- django/backend_practice/urls.py
+++ django/backend_practice/urls.py
@@ -20,16 +20,16 @@ from conduit import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', views.create_user),
-    path('api/users/login/', views.login),
-    path('api/user/', views.current_user),
-    path('api/profiles/<str:username>/', views.get_profile_by_username),
-    path('api/profiles/<str:username>/follow/', views.follow_unfollow_user_by_username),
-    path('api/articles/', views.get_create_articles),
+    path('api/users', views.create_user),
+    path('api/users/login', views.login),
+    path('api/user', views.current_user),
+    path('api/profiles/<str:username>', views.get_profile_by_username),
+    path('api/profiles/<str:username>/follow', views.follow_unfollow_user_by_username),
+    path('api/articles', views.get_create_articles),
     path('api/articles/feed', views.get_articles_feed),
-    path('api/articles/<slug:slug>/', views.get_update_delete_article),
-    path('api/articles/<slug:slug>/comments/', views.get_addto_article_comments),
-    path('api/articles/<slug:slug>/comments/<int:id>/', views.delete_article_comment),
+    path('api/articles/<slug:slug>', views.get_update_delete_article),
+    path('api/articles/<slug:slug>/comments', views.get_addto_article_comments),
+    path('api/articles/<slug:slug>/comments/<int:id>', views.delete_article_comment),
     path('api/articles/<slug:slug>/favorite', views.create_delete_article_favorite),
-    path('api/tags/', views.get_tags)
+    path('api/tags', views.get_tags)
 ]
