e31b7f36187cf1b4456df1e8995ea017399790c8
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-26 14:58:29 -0400

    change email field in User model to use EmailField for better validation

--- django/conduit/models.py
+++ django/conduit/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
     username = models.CharField(unique=True)
-    email = models.CharField(unique=True)
+    email = models.EmailField(unique=True)
     bio = models.CharField(max_length=150, null=True)
     image = models.CharField(max_length=150, null=True)
     following = models.ManyToManyField("self", symmetrical=False)
