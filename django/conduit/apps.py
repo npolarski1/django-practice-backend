4801a3c748bb85e72a63b67dea7101c512175830
Nathan Polarski <nathaniel.polarski@ouboces.org>
2026-08-27 15:31:18 -0400

    refactor Serializer initialization to use custom blank error message

--- django/conduit/apps.py
+++ django/conduit/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
-
+from rest_framework.serializers import Serializer
 
 class ConduitConfig(AppConfig):
     name = 'conduit'
+
+    def ready(self):
+        # update Serializer __init__ so that it uses our custom 'blank' error message
+        original__init = Serializer.__init__
+
+        def updated_init(self, *args, **kwargs):
+            original__init(self, *args, **kwargs)
+
+            CUSTOM_BLANK_MESSAGE = "can't be blank"
+
+            for field in self.fields.values():
+                
+                field.error_messages["blank"] = CUSTOM_BLANK_MESSAGE
+
+        Serializer.__init__ = updated_init
